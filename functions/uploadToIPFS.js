275620d66e48b3253b86679cec3e5c9842e72220
Mitch Malinin <mitchmalinin@gmail.com>
2023-05-21 20:12:58 -0400

    ipfs function working

--- functions/uploadToIPFS.js
+++ functions/uploadToIPFS.js
@@ -1,53 +1,30 @@
-const ipfsClient = require("ipfs-http-client")
+require("dotenv").config()
 
-const authorization =
+const auth =
   "Basic " +
-  Buffer.from(
-    process.env.projectId + ":" + proccess.env.projectSecret
-  ).toString("base64")
+  Buffer.from(process.env.projectId + ":" + process.env.projectSecret).toString(
+    "base64"
+  )
 
 exports.handler = async function (event, context) {
-  const ipfs = ipfsClient({
-    url: "https://ipfs.infura.io:5001",
+  const { create } = await import("ipfs-http-client")
+
+  const client = await create({
+    host: "ipfs.infura.io",
+    port: 5001,
+    protocol: "https",
     headers: {
-      authorization,
+      authorization: auth,
     },
   })
 
-  if (event.httpMethod !== "POST") {
-    return {
-      statusCode: 405,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "POST, GET, PUT, DELETE",
-      },
-      body: "Method Not Allowed",
-    }
-  }
-
-  const file = Buffer.from(JSON.parse(event.body).image, "base64")
-
-  let result
-  try {
-    result = await ipfs.pin.add(file)
-  } catch (error) {
-    console.error("Error while uploading to IPFS:", error)
-    return {
-      statusCode: 500,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "POST, GET, PUT, DELETE",
-      },
-      body: JSON.stringify({ error: "Error while uploading to IPFS" }),
-    }
-  }
-
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "POST, GET, PUT, DELETE",
-    },
-    body: JSON.stringify({ hash: result.path }),
-  }
+  client
+    .add("Hello World")
+    .then((res) => {
+      console.log("working")
+      console.log(res)
+    })
+    .catch((err) => {
+      console.log("error", err)
+    })
 }
